4505cf11025f65f43d1059ff5d1a4ce9a62e0350
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-04-01 10:13:18 +0100

    nested links,relative routes

--- src/components/Host/HostVanDetail.js
+++ src/components/Host/HostVanDetail.js
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 export default function HostVanDetail() {
-    return <h1>Host Van Detail Page</h1>;
+    const [van, setVan] = useState(null); //({})
+    const params = useParams([]);
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${params.id}`)
+            .then((res) => res.json())
+            .then((data) => setVan(data.vans));
+    }, [params.id]);
+    // console.log(params)
+
+    if (!van) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+            <div className='host-van-detail-layout-container'>
+                <div className='host-van-detail'>
+                    <img src={van.imageUrl} />
+                    <div className='host-van-detail-info-text'>
+                        <i className={`van-type van-type-${van.type}`}>
+                            {van.type}
+                        </i>
+                        <h3>{van.name}</h3>
+                        <h4>${van.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/components/Vans/VanDetail.js
+++ src/components/Vans/VanDetail.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function VanDetail() {
-    const [van, setVan] = useState([]);
+    const [van, setVan] = useState(null);
     const params = useParams();
     // console.log(params);
 
